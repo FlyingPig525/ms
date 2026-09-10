f7ba650add20a48df31412ab9ba60f8bbc273990
FlyingPig525 <66709612+FlyingPig525@users.noreply.github.com>
2026-09-10 16:31:42 -0400

    add some comments to the ui code

--- src/ui.zig
+++ src/ui.zig
@@ -1,8 +1,23 @@
 const std = @import("std");
 const rl = @import("raylib");
 
+/// The building block of all UI.
+///
+/// All `Node`s derive themselves from a "manager," a struct that manages
+/// the data and handles the events passed through a `Node`.
+/// Managers are generally allocated in memory, though they don't have to be. If they are, their lifetime
+/// is directly tied with the lifetime of the `Node`.
+///
+/// Each `Node` has children, which are also `Node`s. This creates a UI graph that is traversed to draw,
+/// handle input, and more. The UI graph begins with a top-level `Node`, generally a `RootNode`. These
+/// `Node`s provide the majority of functionality to its children. They dictate how things are drawn
+/// to the screen, how and what events are passed to children, and the lifetime of the entire graph.
+/// Once the top-level `Node` is deinitialized, the entire graph is pruned.
+///
+/// Based loosely on cocos2d-x (from my experience with geometry dash mods. I don't actually know
+/// the inner workings of cocos2d-x. I don't even remember if there is a "-" before the "x").
 pub const Node = struct {
-    self: *anyopaque,
+    manager: *anyopaque,
     frozen: bool,
     vtable: *const VTable,
     tools: *const DrawTools,
@@ -11,9 +26,9 @@ pub const Node = struct {
     parent: *Node,
     gpa: std.mem.Allocator,
 
-    pub fn init(gpa: std.mem.Allocator, self: *anyopaque, space: Space, vtable: *const VTable) !*Node {
+    pub fn init(gpa: std.mem.Allocator, manager: *anyopaque, space: Space, vtable: *const VTable) !*Node {
         var node = try gpa.create(Node);
-        node.self = self;
+        node.manager = manager;
         node.frozen = false;
         node.vtable = vtable;
         node.tools = &.default;
@@ -23,22 +38,28 @@ pub const Node = struct {
         return node;
     }
 
+    /// Recursively deinitializes this node and all children in the graph of this node.
+    /// Fires the `deinit` event on this node and all children.
     pub fn deinit(this: *Node) void {
         for (this.children.items) |child| {
             child.deinit();
         }
-        this.vtable.deinit(this.self);
+        this.vtable.deinit(this.manager);
         this.children.deinit(this.gpa);
         this.gpa.destroy(this);
     }
 
+    /// Adds `child` to this node's children. Tries to fire the `parented` event on the child and the `add_child`
+    /// event on the the parent (this node).
     pub fn addChild(this: *Node, child: *Node) !void {
         try this.children.append(this.gpa, child);
         child.parent = this;
-        if (child.vtable.parented) |i| try i(child.self, child, this);
-        if (this.vtable.add_child) |i| try i(this.self, this, child);
+        if (child.vtable.parented) |i| try i(child.manager, child, this);
+        if (this.vtable.add_child) |i| try i(this.manager, this, child);
     }
 
+    /// Removes and deinitializes the child at index `i` and moves all children of a higher index down to fill
+    /// the spot.
     pub fn removeChild(this: *Node, i: usize) void {
         const child = this.children.orderedRemove(i);
         child.deinit();
@@ -57,6 +78,10 @@ pub const Node = struct {
         }
     };
 
+    /// Provides functions for drawing relative to node-space. A top-level `Node`, such as `RootNode`, should
+    /// provide its own `DrawTools` and do the actual drawing to the screen.
+    ///
+    /// Calls to these functions will bubble up to the root node.
     pub const DrawTools = struct {
         rect: *const fn (node: *Node, rect: rl.Rectangle, color: rl.Color) void,
         rect_lines: *const fn (node: *Node, rect: rl.Rectangle, thickness: f32, color: rl.Color) void,
@@ -74,6 +99,10 @@ pub const Node = struct {
             .measure_text = bubbleMeasureText,
         };
 
+        // Bubble versions of the node draw functions draw to the absolute space of this node.
+        //
+        // For example, if the node has a size `.{ .x = 128, .y = 64 }`, the center would be `.{ .x = 64, .y = 32 }`.
+
         fn bubbleMeasureText(this: *Node, font: rl.Font, text: [:0]const u8, font_size: f32, spacing: f32) rl.Vector2 {
             return this.parent.tools.measure_text(this.parent, font, text, font_size, spacing);
         }
@@ -83,9 +112,6 @@ pub const Node = struct {
             this.parent.tools.text(this.parent, font, text, true_pos, font_size, spacing, tint);
         }
 
-        /// Bubble versions of the node draw functions draw to the absolute space of this node.
-        ///
-        /// For example, if the node has a size `.{ .x = 128, .y = 64 }`, the center would be `.{ .x = 64, .y = 32 }`.
         fn bubbleRect(this: *Node, rect: rl.Rectangle, color: rl.Color) void {
             const true_rect: rl.Rectangle = .{
                 .x = rect.x + this.space.offset.x,
@@ -120,8 +146,8 @@ pub const Node = struct {
 
     /// Draws a rectangle in the space of this node
     ///
-    /// Values should be expressed as a fraction of that value of this node. For example, to draw to the center of this node,
-    /// set the x and y to 0.5
+    /// `rect` values should be expressed as a fraction of that value of this node. For example, to draw
+    /// to the center of this node, set the x and y to 0.5
     pub fn drawRect(this: *Node, rect: rl.Rectangle, color: rl.Color) void {
         const true_rect: rl.Rectangle = .{
             .x = (rect.x * this.space.size.x) + this.space.offset.x,
@@ -134,7 +160,7 @@ pub const Node = struct {
 
     /// Draws a rectangle outline in the space of this node
     ///
-    /// Values, except thickness, should be expressed as a fraction of that value of this node. For example, to draw
+    /// `rect` values should be expressed as a fraction of that value of this node. For example, to draw
     /// to the center of this node, set the x and y to 0.5
     pub fn drawRectLines(this: *Node, rect: rl.Rectangle, thickness: f32, color: rl.Color) void {
         const true_rect: rl.Rectangle = .{
@@ -148,7 +174,7 @@ pub const Node = struct {
 
     /// Draws a line in the space of this node
     ///
-    /// Values, except thickness, should be expressed as a fraction of that value of this node. For example, to draw
+    /// `start` and `end` values should be expressed as a fraction of that value of this node. For example, to draw
     /// to the center of this node, set the x and y to 0.5
     pub fn drawLine(this: *Node, start: rl.Vector2, end: rl.Vector2, thickness: f32, color: rl.Color) void {
         const true_start = start.multiply(this.space.size).add(this.space.offset);
@@ -158,18 +184,24 @@ pub const Node = struct {
 
     /// Draws a circle in the space of this node
     ///
-    /// Values, except for radius, should be expressed as a fraction of that value of this node. For example, to draw
+    /// `center` values should be expressed as a fraction of that value of this node. For example, to draw
     /// to the center of this node, set the x and y to 0.5
     pub fn drawCircle(this: *Node, center: rl.Vector2, radius: f32, color: rl.Color) void {
         const true_center = center.multiply(this.space.size).add(this.space.offset);
         this.parent.tools.circle(this.parent, true_center, radius, color);
     }
 
+    /// Draws a circle in the space of this node
+    ///
+    /// `pos` values should be expressed as a fraction of that value of this node. For example, to draw
+    /// to the center of this node, set the x and y to 0.5
     pub fn drawText(this: *Node, font: rl.Font, text: [:0]const u8, pos: rl.Vector2, font_size: f32, spacing: f32, tint: rl.Color) void {
         const true_pos = pos.multiply(this.space.size).add(this.space.offset);
         this.parent.tools.text(this.parent, font, text, true_pos, font_size, spacing, tint);
     }
 
+    /// Measures the size of text. Bubbles up until it finds an implementation of `DrawTools.measure_text`, generally
+    /// a `RootNode`, returning the size of the text in terms of the `RootNode`'s scale-space.
     fn measureText(this: *Node, font: rl.Font, text: [:0]const u8, font_size: f32, spacing: f32) rl.Vector2 {
         return this.parent.tools.measure_text(this.parent, font, text, font_size, spacing);
     }
@@ -179,7 +211,7 @@ pub const Node = struct {
     /// If this node's `vtable` contains a `calculate_size` member, it calls that instead.
     pub fn calculateSize(this: *Node) !void {
         if (this.vtable.calculate_size) |i| {
-            this.space.size = try i(this.self, this);
+            this.space.size = try i(this.manager, this);
         } else {
             var size: rl.Vector2 = .init(0, 0);
             var largest_offset: rl.Vector2 = .init(0, 0);
@@ -202,14 +234,33 @@ pub const Node = struct {
     pub const Propagation = enum(u1) { propagate, dont_propagate };
 
     pub const VTable = struct {
+        /// Fires whenever a mouse button is pressed and the cursor intersects with the node's space.
+        /// `relative_pos` -- the position of the mouse cursor, minus the node's absolute offset.
+        ///
+        /// Returns whether to propagate this event to children.
         on_click: ?(*const fn (this: *anyopaque, node: *Node, button: rl.MouseButton, relative_pos: rl.Vector2) anyerror!Propagation) = null,
+        /// Fires whenever a keyboard key is pressed.
+        ///
+        /// Returns whether to propagate this event to children.
         on_input: ?(*const fn (this: *anyopaque, node: *Node, key: rl.KeyboardKey) anyerror!Propagation) = null,
+        /// Fires whenever a child is added to this node.
         add_child: ?(*const fn (this: *anyopaque, node: *Node, child: *Node) anyerror!void) = null,
+        /// Fires every frame, allowing a node to draw to the screen using the draw functions available through
+        /// `node`.
         draw: ?(*const fn (this: *anyopaque, node: *Node) anyerror!void) = null,
+        /// Fires before every frame.
         tick: ?(*const fn (this: *anyopaque, node: *Node, dt: f32) anyerror!void) = null,
+        /// Fires when this node becomes a child of another node.
         parented: ?(*const fn (this: *anyopaque, node: *Node, parent: *Node) anyerror!void) = null,
+        /// Returns the size of this node. This is automatically called recursively on the node graph when
+        /// this node or a parent node is attached to a `RootNode`.
+        ///
         /// If this member has a value, the function pointed to must call `calculateSize` on each of its children.
         calculate_size: ?(*const fn (this: *anyopaque, node: *Node) anyerror!rl.Vector2) = null,
+        /// Fires when the node is removed from its graph, causing it, and all of its children, to be deinitialized.
+        /// Allows automatic cleanup of a custom node's resources.
+        ///
+        /// Any node that allocates memory or requires any other cleanup after use should provide a `deinit` function.
         deinit: *const fn (this: *anyopaque) void = nopDeinit,
 
         pub const nop: VTable = .{};
@@ -219,7 +270,7 @@ pub const Node = struct {
 
     pub fn onClick(this: *Node, button: rl.MouseButton, relative_pos: rl.Vector2) !void {
         if (this.frozen) return;
-        if (this.vtable.on_click != null and try this.vtable.on_click.?(this.self, this, button, relative_pos) != .propagate) return;
+        if (this.vtable.on_click != null and try this.vtable.on_click.?(this.manager, this, button, relative_pos) != .propagate) return;
         for (this.children.items) |child| {
             const child_rect = rl.Rectangle{
                 .x = child.space.offset.x,
@@ -236,27 +287,31 @@ pub const Node = struct {
 
     pub fn onInput(this: *Node, key: rl.KeyboardKey) !void {
         if (this.frozen) return;
-        if (this.vtable.on_input != null and try this.vtable.on_input.?(this.self, this, key) != .propagate) return;
+        if (this.vtable.on_input != null and try this.vtable.on_input.?(this.manager, this, key) != .propagate) return;
         for (this.children.items) |child| {
             try child.onInput(key);
         }
     }
 
     pub fn draw(this: *Node) !void {
-        if (this.vtable.draw) |i| try i(this.self, this);
+        if (this.vtable.draw) |i| try i(this.manager, this);
         for (this.children.items) |child| {
             try child.draw();
         }
     }
 
     pub fn tick(this: *Node, dt: f32) !void {
-        if (this.vtable.tick) |i| try i(this.self, this, dt);
+        if (this.vtable.tick) |i| try i(this.manager, this, dt);
         for (this.children.items) |child| {
             try child.tick(dt);
         }
     }
 };
 
+/// The general-purpose top-level node. Allows the user to specify a custom screen size that will be scaled to fit
+/// the actual screen size. It also provides all `DrawTools` functions to draw to the screen.
+///
+/// Whenever a node is added as a child to a `RootNode`, it calculates the size of the new child node.
 pub const RootNode = struct {
     screen_size: rl.Vector2,
     true_size: rl.Vector2,
@@ -274,7 +329,7 @@ pub const RootNode = struct {
     };
 
     pub fn drawRect(node: *Node, rect: rl.Rectangle, color: rl.Color) void {
-        const this: *RootNode = @ptrCast(@alignCast(node.self));
+        const this: *RootNode = @ptrCast(@alignCast(node.manager));
         const true_rect: rl.Rectangle = .{
             .x = (rect.x / this.screen_size.x) * this.true_size.x,
             .y = (rect.y / this.screen_size.y) * this.true_size.y,
@@ -285,7 +340,7 @@ pub const RootNode = struct {
     }
 
     pub fn drawRectLines(node: *Node, rect: rl.Rectangle, thickness: f32, color: rl.Color) void {
-        const this: *RootNode = @ptrCast(@alignCast(node.self));
+        const this: *RootNode = @ptrCast(@alignCast(node.manager));
         const true_rect: rl.Rectangle = .{
             .x = (rect.x / this.screen_size.x) * this.true_size.x,
             .y = (rect.y / this.screen_size.y) * this.true_size.y,
@@ -296,7 +351,7 @@ pub const RootNode = struct {
     }
 
     pub fn drawLine(node: *Node, start: rl.Vector2, end: rl.Vector2, thickness: f32, color: rl.Color) void {
-        const this: *RootNode = @ptrCast(@alignCast(node.self));
+        const this: *RootNode = @ptrCast(@alignCast(node.manager));
         const true_start: rl.Vector2 = .{
             .x = (start.x / this.screen_size.x) * this.true_size.x,
             .y = (start.y / this.screen_size.y) * this.true_size.y,
@@ -309,7 +364,7 @@ pub const RootNode = struct {
     }
 
     pub fn drawCircle(node: *Node, center: rl.Vector2, radius: f32, color: rl.Color) void {
-        const this: *RootNode = @ptrCast(@alignCast(node.self));
+        const this: *RootNode = @ptrCast(@alignCast(node.manager));
         const true_center: rl.Vector2 = .{
             .x = (center.x / this.screen_size.x) * this.true_size.x,
             .y = (center.y / this.screen_size.y) * this.true_size.y,
@@ -318,7 +373,7 @@ pub const RootNode = struct {
     }
 
     pub fn drawText(node: *Node, font: rl.Font, text: [:0]const u8, pos: rl.Vector2, font_size: f32, spacing: f32, tint: rl.Color) void {
-        const this: *RootNode = @ptrCast(@alignCast(node.self));
+        const this: *RootNode = @ptrCast(@alignCast(node.manager));
         const true_pos: rl.Vector2 = .{
             .x = (pos.x / this.screen_size.x) * this.true_size.x,
             .y = (pos.y / this.screen_size.y) * this.true_size.y,
@@ -327,7 +382,7 @@ pub const RootNode = struct {
     }
 
     pub fn measureText(node: *Node, font: rl.Font, text: [:0]const u8, font_size: f32, spacing: f32) rl.Vector2 {
-        const this: *RootNode = @ptrCast(@alignCast(node.self));
+        const this: *RootNode = @ptrCast(@alignCast(node.manager));
         const vec = rl.measureTextEx(font, text, font_size, spacing);
         return vec.divide(this.true_size).multiply(this.screen_size);
     }
@@ -343,14 +398,15 @@ pub const RootNode = struct {
     }
 };
 
+/// A node that draw text to the screen.
 pub const TextNode = struct {
     gpa: std.mem.Allocator,
     text: [:0]const u8,
     font_size: f32,
     font: rl.Font,
     tint: rl.Color,
-    ref_count: usize,
 
+    /// Dupes `text` into ram
     pub fn initDefault(gpa: std.mem.Allocator, text: [:0]const u8, font_size: f32, tint: rl.Color) !*TextNode {
         const node = try gpa.create(TextNode);
         node.gpa = gpa;
@@ -390,6 +446,7 @@ pub const TextNode = struct {
     }
 };
 
+/// An empty node. No different than just initializing a `Node` on its own.
 pub const EmptyNode = struct {
     space: Node.Space,
 
@@ -398,6 +455,7 @@ pub const EmptyNode = struct {
     }
 };
 
+/// A node that automatically lays out its children to follow a simple scheme.
 pub const LayoutNode = struct {
     gap: f32,
     direction: Direction,
@@ -436,8 +494,12 @@ pub const LayoutNode = struct {
     };
 
     pub const Flow = enum {
+        /// Horizontal: left. Vertical: top.
+        ///
         /// Requires one loop through node children
         start,
+        /// Horizontal: right. Vertical: bottom.
+        ///
         /// Requires two loops through node children
         end,
     };
